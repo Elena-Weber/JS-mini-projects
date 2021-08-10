4c1f7bbd10559d4667c3c74d2503b007ac0f600a
Elena-Weber <crochetmiracles@gmail.com>
2021-08-09 21:10:36 -0400

    add second input

--- calc/calc.js
+++ calc/calc.js
@@ -1,35 +1,58 @@
-let initial = '';
+// let initial = '';
 
-const userInput = document.getElementById('user_input');
+// const userInput = document.getElementById('user_input');
 
-const result = document.getElementById('result');
+// const result = document.getElementById('result');
 
-function calc(val) {
-    initial += val;
-    userInput.value = initial;
+// function calc(val) {
+//     initial += val;
+//     userInput.value = initial;
+// }
+
+// function remove() {
+//     initial = initial.substring(0, initial.length - 1);
+//     userInput.value = initial;
+// }
+
+// function execute() {
+//     if (initial.length === 0 || initial === "") {
+//         alert("Oops! There's nothing to calculate. Please, try again!");
+//         return;
+//     }
+//     try {
+//         answer = eval(initial);
+//         result.value = answer;
+//     }
+//     catch(err) {
+//         alert("There's an error in your input. Please, try again!");
+//     }
+// }
+
+// function reset() {
+//     userInput.value = '';
+//     result.value = '';
+//     initial = '';
+// }
+
+const userinput = document.getElementById('userinput');
+
+let result = document.getElementById('result');
+
+let calc = (val) => {
+    userinput.value += val;
 }
-
-function remove() {
-    initial = initial.substring(0, initial.length - 1);
-    userInput.value = initial;
-}
-
-function execute() {
-    if (initial.length === 0 || initial === "") {
-        alert("Oops! There's nothing to calculate. Please, try again!");
-        return;
-    }
+let execute = () => {
     try {
-        answer = eval(initial);
-        result.value = answer;
+        result.value = eval(userinput.value)
     }
     catch(err) {
         alert("There's an error in your input. Please, try again!");
     }
 }
-
 function reset() {
-    userInput.value = '';
-    result.value = '';
-    initial = '';
+        userinput.value = '';
+        result.value = '';
+    }
+function remove() {
+    userinput.value = userinput.value.slice(0, -1);
 }
