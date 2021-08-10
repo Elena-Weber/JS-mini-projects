e677cce6eebdb5bed9f2126e67fc4c668636345c
Elena-Weber <crochetmiracles@gmail.com>
2021-08-09 22:34:23 -0400

    remove unnecessary code in calc

--- calc/calc.js
+++ calc/calc.js
@@ -1,65 +1,29 @@
-// let initial = '';
-
-// const userInput = document.getElementById('user_input');
-
-// const result = document.getElementById('result');
-
-// function calc(val) {
-//     initial += val;
-//     userInput.value = initial;
-// }
-
-// function remove() {
-//     initial = initial.substring(0, initial.length - 1);
-//     userInput.value = initial;
-// }
-
-// function execute() {
-//     if (initial.length === 0 || initial === "") {
-//         alert("Oops! There's nothing to calculate. Please, try again!");
-//         return;
-//     }
-//     try {
-//         answer = eval(initial);
-//         result.value = answer;
-//     }
-//     catch(err) {
-//         alert("There's an error in your input. Please, try again!");
-//     }
-// }
-
-// function reset() {
-//     userInput.value = '';
-//     result.value = '';
-//     initial = '';
-// }
-
-let userinput = document.getElementById('userinput');
+let userInput = document.getElementById('userInput');
 
 let result = document.getElementById('result');
 
 let calc = (val) => {
-    userinput.value += val;
+    userInput.value += val;
 }
 let execute = () => {
-    if (userinput.value.length === 0 || userinput.value === "") {
+    if (userInput.value.length === 0 || userInput.value === "") {
         alert("Oops! There's nothing to calculate. Please, try again!");
         return;
     }
     try {
-        result.value = eval(userinput.value);
+        result.value = eval(userInput.value);
     }
     catch(err) {
         alert("There's an error in your input. Please, try again!");
     }
 }
 function reset() {
-        userinput.value = '';
+    userInput.value = '';
         result.value = '';
     }
 function remove() {
-    userinput.value = userinput.value.slice(0, -1);
-    if (userinput.value.length === 0 || userinput.value === "") {
+    userInput.value = userInput.value.slice(0, -1);
+    if (userInput.value.length === 0 || userInput.value === "") {
         result.value = '';
     }
 }
