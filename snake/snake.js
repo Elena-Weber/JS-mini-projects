d7c8e20ecb7167be7d4ad954fa36df295826235f
Elena-Weber <crochetmiracles@gmail.com>
2021-08-10 22:49:08 -0400

    finish snake

--- snake/snake.js
+++ snake/snake.js
@@ -190,15 +190,15 @@ drawGame();
 const spans = document.querySelectorAll('.word span');
 
 spans.forEach((span, idx) => {
-  span.addEventListener('click', (e) => {
-    e.target.classList.add('active');
-  });
-  span.addEventListener('animationend', (e) => {
-    e.target.classList.remove('active');
-  });
-  
+    span.addEventListener('click', (e) => {
+        e.target.classList.add('active');
+    });
+    span.addEventListener('animationend', (e) => {
+        e.target.classList.remove('active');
+    });
+
   // Initial animation
-  setTimeout(() => {
-    span.classList.add('active');
-  }, 750 * (idx+1))
-});
+    setTimeout(() => {
+        span.classList.add('active');
+    }, 750 * (idx+1))
+})
