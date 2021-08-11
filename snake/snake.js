af533875e28950c1522b97c4936c4c6ab83d3a9c
Elena-Weber <crochetmiracles@gmail.com>
2021-08-10 21:49:24 -0400

    add text styling

--- snake/snake.js
+++ snake/snake.js
@@ -184,3 +184,21 @@ function keyDown(event) {
 }
 
 drawGame();
+
+// title
+
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+  span.addEventListener('click', (e) => {
+    e.target.classList.add('active');
+  });
+  span.addEventListener('animationend', (e) => {
+    e.target.classList.remove('active');
+  });
+  
+  // Initial animation
+  setTimeout(() => {
+    span.classList.add('active');
+  }, 750 * (idx+1))
+});
