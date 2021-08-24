3d2e35f193c4eec7fb3fe88a98a005350bbbd9bf
Elena-Weber <crochetmiracles@gmail.com>
2021-08-24 12:46:25 -0400

    fix bug with second countdown

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,9 +1,8 @@
-function countDown() {
-    const xDate = new Date("December 25, 2021 00:00");
-    const nyDate = new Date("December 31, 2021 00:00");
+function countDownX() {
+    // const xDate = new Date("December 25, 2021 00:00");
+    const xDate = new Date("August 24, 2021 12:46");
     const now = new Date();
     const diffX = xDate - now;
-    const diffNY = nyDate - now;
 
     const msSec = 1000;
     const msMinute = 60 * 1000;
@@ -27,16 +26,22 @@ function countDown() {
         document.querySelector(".minutesX").textContent = 0;
         document.querySelector(".hoursX").textContent = 0;
         document.querySelector(".daysX").textContent = 0;
-        clearInterval(timerID);
+        clearInterval(timerX);
         merryXmas();
-    } else if (diffNY <= 0) {
-        document.querySelector(".secondsNY").textContent = 0;
-        document.querySelector(".minutesNY").textContent = 0;
-        document.querySelector(".hoursNY").textContent = 0;
-        document.querySelector(".daysNY").textContent = 0;
-        clearInterval(timerID);
-        happyNewYear();
     }
+
+}
+
+function countDownNY() {
+    // const nyDate = new Date("January 1, 2022 00:00");
+    const nyDate = new Date("August 24, 2021 12:46");
+    const now = new Date();
+    const diffNY = nyDate - now;
+
+    const msSec = 1000;
+    const msMinute = 60 * 1000;
+    const msHour = 60 * 60 * 1000;
+    const msDay = 24 * 60 * 60 * 1000;
 
     const displaySecondsNY = Math.floor((diffNY%msMinute)/msSec);
     document.querySelector(".secondsNY").textContent = displaySecondsNY;
@@ -50,6 +55,14 @@ function countDown() {
     const displayDaysNY = Math.floor(diffNY/msDay);
     document.querySelector(".daysNY").textContent = displayDaysNY;
 
+    if (diffNY <= 0) {
+        document.querySelector(".secondsNY").textContent = 0;
+        document.querySelector(".minutesNY").textContent = 0;
+        document.querySelector(".hoursNY").textContent = 0;
+        document.querySelector(".daysNY").textContent = 0;
+        clearInterval(timerNY);
+        happyNewYear();
+    }
 }
 
 function merryXmas() {
@@ -62,4 +75,5 @@ function happyNewYear() {
     heading.textContent = "Happy New Year!";
 }
 
-let timerID = setInterval(countDown, 1000);
+let timerX = setInterval(countDownX, 1000);
+let timerNY = setInterval(countDownNY, 1000);
