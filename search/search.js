27f7d62031f32a9b0c2af18c23fe4e7442ef2d9f
Elena-Weber <crochetmiracles@gmail.com>
2021-08-30 12:50:47 -0400

    change caret color in search

--- search/search.js
+++ search/search.js
@@ -1,12 +1,9 @@
 const search = document.querySelector("#search");
 const perf = document.querySelectorAll(".perf");
-console.log(perf.length);
 
 search.addEventListener("keyup", (event) => {
     const word = event.target.value.toLowerCase();
-
     perf.forEach(item => {
         item.querySelector("h4").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none");
     })
-    
 });
