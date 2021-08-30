705866cd2169cfb675d86ae258e7168ef8611967
Elena-Weber <crochetmiracles@gmail.com>
2021-08-29 20:35:31 -0400

    add 100 perfumes

--- search/search.js
+++ search/search.js
@@ -1,13 +1,12 @@
 const search = document.querySelector("#search");
 const perf = document.querySelectorAll(".perf");
+console.log(perf.length);
 
 search.addEventListener("keyup", (event) => {
     const word = event.target.value.toLowerCase();
 
     perf.forEach(item => {
         item.querySelector("h4").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none");
-        console.log();
     })
     
-})
-
+});
