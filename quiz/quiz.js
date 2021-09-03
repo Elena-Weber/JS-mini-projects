21c35e263b4152d500db50d911e4af25d5b648cf
Elena-Weber <crochetmiracles@gmail.com>
2021-09-03 18:38:01 -0400

    start work on quiz

---
+++ quiz/quiz.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", result);
+
+function result(event) {
+    event.preventDefault();
+
+let points = 0;
+if (document.querySelector("#answer1")) {
+    
+}
+
+
+}
