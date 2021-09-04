58a6a9b58f74d62b79aa9823169ae8b2b1be7b5b
Elena-Weber <crochetmiracles@gmail.com>
2021-09-03 21:34:59 -0400

    quiz questions added

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -5,9 +5,124 @@ function result(event) {
     event.preventDefault();
 
 let points = 0;
-if (document.querySelector("#answer1")) {
-    
+if (document.querySelector("#answer1").checked) {
+    points ++;
+    // console.log(points);
 }
-
-
+if (document.querySelector("#answer2").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer3").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer4").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer5").checked) {
+    points ++;
+    console.log(points);
+}
+if (document.querySelector("#answer6").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer7").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer8").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer9").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer10").checked) {
+    points ++;
+    console.log(points);
+}
+if (document.querySelector("#answer11").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer12").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer13").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer14").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer15").checked) {
+    points ++;
+    console.log(points);
+}
+if (document.querySelector("#answer16").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer17").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer18").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer19").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer20").checked) {
+    points ++;
+    console.log(points);
+}
+if (document.querySelector("#answer21").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer22").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer23").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer24").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer25").checked) {
+    points ++;
+    console.log(points);
+}
+if (document.querySelector("#answer26").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer27").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer28").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer29").checked) {
+    points ++;
+    // console.log(points);
+}
+if (document.querySelector("#answer30").checked) {
+    points ++;
+    console.log(points);
+}
 }
