1e853740bcd0aab51735c047cf98c6b583837c5a
Elena-Weber <crochetmiracles@gmail.com>
2021-09-04 21:28:33 -0400

    tip calc working well

--- tip_calc/tip_calc.js
+++ tip_calc/tip_calc.js
@@ -1,15 +1,9 @@
-const btn = document.querySelector("#calc");
-btn.addEventListener("click", calculate);
-
-const addTip = document.querySelector("#add_tip");
-addTip.addEventListener("click", tipOptions);
-
 const reset = document.querySelector("#reset");
 reset.addEventListener("click", () => {
     location.reload();
 });
 
-function calculate(event) {
+let calculate = (event) => {
     event.preventDefault();
     const bill = document.querySelector("#bill").value;
     const people = document.querySelector("#people").value;
@@ -33,11 +27,17 @@ function calculate(event) {
 
 }
 
-function tipOptions(event) {
+const btn = document.querySelector("#calc");
+btn.addEventListener("click", calculate);
+
+let tipOptions = (event) => {
     event.preventDefault();
     const tip = document.querySelector("#tip");
     tip.style.display = "block";
 }
+
+const addTip = document.querySelector("#add_tip");
+addTip.addEventListener("click", tipOptions);
 
 const checkbox = document.querySelector("#checkbox");
 const checkedBox = document.querySelector("#checkedBox");
@@ -55,108 +55,101 @@ result.addEventListener("click", (event) => {
     let points = 0;
     if (document.querySelector(".food1").checked) {
         points += 1;
-        console.log(points);
     }
     if (document.querySelector(".drink1").checked) {
         points += 1;
-        console.log(points);
     }
     if (document.querySelector(".service1").checked) {
         points += 1;
-        console.log(points);
     }
     if (document.querySelector(".atmosphere1").checked) {
         points += 1;
-        console.log(points);
     }
     if (document.querySelector(".impression1").checked) {
         points += 1;
-        console.log(points);
     }
 
     if (document.querySelector(".food2").checked) {
         points += 2;
-        console.log(points);
     }
     if (document.querySelector(".drink2").checked) {
         points += 2;
-        console.log(points);
     }
     if (document.querySelector(".service2").checked) {
         points += 2;
-        console.log(points);
     }
     if (document.querySelector(".atmosphere2").checked) {
         points += 2;
-        console.log(points);
     }
     if (document.querySelector(".impression2").checked) {
         points += 2;
-        console.log(points);
     }
 
     if (document.querySelector(".food3").checked) {
         points += 3;
-        console.log(points);
     }
     if (document.querySelector(".drink3").checked) {
         points += 3;
-        console.log(points);
     }
     if (document.querySelector(".service3").checked) {
         points += 3;
-        console.log(points);
     }
     if (document.querySelector(".atmosphere3").checked) {
         points += 3;
-        console.log(points);
     }
     if (document.querySelector(".impression3").checked) {
         points += 3;
-        console.log(points);
     }
 
     if (document.querySelector(".food4").checked) {
         points += 4;
-        console.log(points);
     }
     if (document.querySelector(".drink4").checked) {
         points += 4;
-        console.log(points);
     }
     if (document.querySelector(".service4").checked) {
         points += 4;
-        console.log(points);
     }
     if (document.querySelector(".atmosphere4").checked) {
         points += 4;
-        console.log(points);
     }
     if (document.querySelector(".impression4").checked) {
         points += 4;
-        console.log(points);
     }
 
     if (document.querySelector(".food5").checked) {
         points += 5;
-        console.log(points);
     }
     if (document.querySelector(".drink5").checked) {
         points += 5;
-        console.log(points);
     }
     if (document.querySelector(".service5").checked) {
         points += 5;
-        console.log(points);
     }
     if (document.querySelector(".atmosphere5").checked) {
         points += 5;
-        console.log(points);
     }
     if (document.querySelector(".impression5").checked) {
         points += 5;
-        console.log(points);
     }
 
-    
+    let tip = '';
+    if (points === 0) {
+        tip = 0;
+    } else if (points > 0 && points <= 5) {
+        tip = "5%";
+    } else if (points > 5 && points <= 10) {
+        tip = "10%";
+    } else if (points > 10 && points <= 15) {
+        tip = "15%";
+    } else if (points > 15 && points <= 20) {
+        tip = "20%";
+    } else if (points > 20 && points <= 24) {
+        tip = "25%";
+    } else {
+        tip = "30% and higher";
+    }
+
+    document.querySelector("#proposed-tip").style.display = "block";
+    document.querySelector("#proposed").textContent = tip;
 })
