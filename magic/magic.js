e50aa8c4a9896e5fd6face3de5719a0985d8a275
Elena-Weber <crochetmiracles@gmail.com>
2021-09-11 20:28:51 -0400

    add answers to magic ball

---
+++ magic/magic.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".btn");
+const answers = document.querySelector("#answers");
+
+const options = ["It is certain.",
+"Reply hazy, try again.",
+"It is decidedly so.",
+"Don't count on it.",
+"Without a doubt.",
+"Ask again later.",
+"Yes definitely.",
+"My reply is no.",
+"You may rely on it.",
+"Better not tell you now.",
+"As I see it, yes.",
+"My sources say no.",
+"Most likely.",
+"Cannot predict now.",
+"Outlook good.",
+"Outlook not so good.",
+"Yes.",
+"Concentrate and ask again.",
+"Signs point to yes.",
+"Very doubtful."
+];
