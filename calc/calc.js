025c351e86ed6148433fce4b3e38314daad23443
Elena-Weber <crochetmiracles@gmail.com>
2021-09-27 21:45:59 -0400

    add pseudo code to calcl

--- calc/calc.js
+++ calc/calc.js
@@ -1,10 +1,12 @@
 let userInput = document.getElementById('userInput');
-
 let result = document.getElementById('result');
 
+// calculations
 let calc = (val) => {
     userInput.value += val;
 }
+
+// validating input
 let execute = () => {
     if (userInput.value.length === 0 || userInput.value === "") {
         alert("Oops! There's nothing to calculate. Please, try again!");
@@ -17,10 +19,14 @@ let execute = () => {
         alert("There's an error in your input. Please, try again!");
     }
 }
+
+// reset calculation
 function reset() {
     userInput.value = '';
         result.value = '';
-    }
+}
+
+// remove last character
 function remove() {
     userInput.value = userInput.value.slice(0, -1);
     if (userInput.value.length === 0 || userInput.value === "") {
