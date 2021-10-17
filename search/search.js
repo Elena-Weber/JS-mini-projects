66dc367a25bb91279ec0e24a4cdd47e68dfe2053
Elena-Weber <crochetmiracles@gmail.com>
2021-10-17 17:44:31 -0400

    add error message to search

--- search/search.js
+++ search/search.js
@@ -1,17 +1,18 @@
-const search = document.querySelector("#search");
-const brand = document.querySelector("#brand");
-const perf = document.querySelectorAll(".perf");
+const search = document.querySelector("#search")
+const brand = document.querySelector("#brand")
+const perf = document.querySelectorAll(".perf")
+const notFound = document.querySelector("#not_found")
 
 search.addEventListener("keyup", (event) => {
     const word = event.target.value.toLowerCase();
     perf.forEach(item => {
-        item.querySelector("h4").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none");
+        item.querySelector("h4").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none") && (notFound.style.display = "block");
     })
 })
 
 brand.addEventListener("keyup", (event) => {
     const word = event.target.value.toLowerCase();
     perf.forEach(item => {
-        item.querySelector("span").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none");
+        item.querySelector("span").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none") && (notFound.style.display = "block");
     })
-});
+})
