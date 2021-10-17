9fe1dafb29d2536ac440ad573e4b3e0b5304dddf
Elena-Weber <crochetmiracles@gmail.com>
2021-10-17 17:30:24 -0400

    search finished

--- search/search.js
+++ search/search.js
@@ -1,9 +1,17 @@
 const search = document.querySelector("#search");
+const brand = document.querySelector("#brand");
 const perf = document.querySelectorAll(".perf");
 
 search.addEventListener("keyup", (event) => {
     const word = event.target.value.toLowerCase();
     perf.forEach(item => {
         item.querySelector("h4").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none");
+    })
+})
+
+brand.addEventListener("keyup", (event) => {
+    const word = event.target.value.toLowerCase();
+    perf.forEach(item => {
+        item.querySelector("span").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none");
     })
 });
