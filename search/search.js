8e93d63875e5fee38ba1e30997f000ea29ac9d3b
Elena-Weber <crochetmiracles@gmail.com>
2021-10-17 18:48:12 -0400

    finish work on search

--- search/search.js
+++ search/search.js
@@ -1,18 +1,9 @@
 const search = document.querySelector("#search")
-const brand = document.querySelector("#brand")
 const perf = document.querySelectorAll(".perf")
-const notFound = document.querySelector("#not_found")
 
 search.addEventListener("keyup", (event) => {
     const word = event.target.value.toLowerCase();
     perf.forEach(item => {
-        item.querySelector("h4").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none") && (notFound.style.display = "block");
-    })
-})
-
-brand.addEventListener("keyup", (event) => {
-    const word = event.target.value.toLowerCase();
-    perf.forEach(item => {
-        item.querySelector("span").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none") && (notFound.style.display = "block");
+        ((item.querySelector("h4").textContent.toLowerCase().includes(word)) || (item.querySelector("span").textContent.toLowerCase().includes(word))) ? (item.style.display = "block") : (item.style.display = "none");
     })
 })
