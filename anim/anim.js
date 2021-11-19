ee09cd8fb0809f273f2684406975111f1be07850
Elena-Weber <crochetmiracles@gmail.com>
2021-11-18 20:04:50 -0500

    change position of squares in anim

--- anim/anim.js
+++ anim/anim.js
@@ -2,16 +2,16 @@ gsap.to(".header", {y: 20, ease: "bounce", duration: 2})
 
 gsap.from("a", {opacity: 0, duration: 3, stagger: 1})
 
-gsap.to(".first", {x: 400, duration: 5, ease: "power1", opacity: 1})
+gsap.to(".first", {x: 100, duration: 5, ease: "power1", opacity: 1})
 
-gsap.to(".second", {x: 425, duration: 3, delay: 1, ease: "power2", opacity: 1})
+gsap.to(".second", {x: 150, duration: 3, delay: 1, ease: "power2", opacity: 1})
 
-gsap.to(".third", {x: 450, duration: 3, delay: 2, ease: "power3", opacity: 1})
+gsap.to(".third", {x: 200, duration: 3, delay: 2, ease: "power3", opacity: 1})
 
-gsap.to(".fourth", {x: 475, duration: 3, delay: 3, ease: "power4", opacity: 1})
+gsap.to(".fourth", {x: 250, duration: 3, delay: 3, ease: "power4", opacity: 1})
 
-gsap.from(".question", {opacity: 0, delay: 4})
-gsap.to(".question", {y: 200, duration: 3, ease: "power1", opacity: 1, delay: 4})
+gsap.from(".question", {x: 200, y: -200, opacity: 0, delay: 4})
+gsap.to(".question", {x: 600, y: 200, duration: 3, ease: "power1", opacity: 1, delay: 4})
 
 gsap.from(".btn", {opacity: 0, delay: 4.5})
 gsap.to(".btn", {x: 70, y: 200, duration: 3, ease: "power1", opacity: 1, delay: 4.5})
