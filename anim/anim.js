51e117d911db66f1fcfaf122cb189af46efa4a79
Elena-Weber <crochetmiracles@gmail.com>
2021-11-24 21:11:23 -0500

    add js to change text in btn in anim

--- anim/anim.js
+++ anim/anim.js
@@ -4,11 +4,11 @@ gsap.from("h2", {opacity: 0, duration: 3, stagger: 1})
 
 gsap.to(".first", {x: 150, duration: 5, delay: 3, ease: "power1", opacity: 1})
 
-gsap.to(".second", {x: 200, duration: 3, delay: 4, ease: "power2", opacity: 1})
+gsap.to(".second", {x: 225, duration: 3, delay: 4, ease: "power2", opacity: 1})
 
-gsap.to(".third", {x: 250, duration: 3, delay: 5, ease: "power3", opacity: 1})
+gsap.to(".third", {x: 300, duration: 3, delay: 5, ease: "power3", opacity: 1})
 
-gsap.to(".fourth", {x: 300, duration: 3, delay: 6, ease: "power4", opacity: 1})
+gsap.to(".fourth", {x: 375, duration: 3, delay: 6, ease: "power4", opacity: 1})
 
 gsap.from(".question", {x: 1000, y: 200, delay: 7})
 gsap.to(".question", {x: 670, y: 200, duration: 3, ease: "power1", opacity: 1, delay: 7})
@@ -18,3 +18,11 @@ gsap.to(".yes-btn", {x: 640, y: 250, duration: 3, ease: "power1", opacity: 1, de
 
 gsap.from(".no-btn", {x: 740, y: 1000, opacity: 0, delay: 8})
 gsap.to(".no-btn", {x: 740, y: 250, duration: 3, ease: "power1", opacity: 1, delay: 8})
+
+let no_btn = document.querySelector(".no-btn")
+no_btn.addEventListener("mouseover", () => {
+    no_btn.textContent = "YES";
+})
+no_btn.addEventListener("mouseout", () => {
+    no_btn.textContent = "NO";
+})
