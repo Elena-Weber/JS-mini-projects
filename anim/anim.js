c0a6a2f52216af531047670a5faff941267151ea
Elena-Weber <crochetmiracles@gmail.com>
2021-11-27 14:33:51 -0500

    smile emoji added

--- anim/anim.js
+++ anim/anim.js
@@ -10,19 +10,22 @@ gsap.to(".third", {x: 300, duration: 3, delay: 5, ease: "power3", opacity: 1})
 
 gsap.to(".fourth", {x: 375, duration: 3, delay: 6, ease: "power4", opacity: 1})
 
+gsap.to("#smile", {x: 130, y: -200, duration: 3, delay: 7, ease: "elastic", rotation: 360, opacity: 1})
+
 gsap.from(".question", {x: 1000, y: 200, delay: 7})
-gsap.to(".question", {x: 670, y: 200, duration: 3, ease: "power1", opacity: 1, delay: 7})
+gsap.to(".question", {x: 630, y: 200, duration: 3, ease: "power1", opacity: 1, delay: 7})
 
-gsap.from(".yes-btn", {x: 640, y: 1000, opacity: 0, delay: 8})
-gsap.to(".yes-btn", {x: 640, y: 250, duration: 3, ease: "power1", opacity: 1, delay: 8})
+gsap.from(".yes-btn", {x: 600, y: 1000, opacity: 0, delay: 8})
+gsap.to(".yes-btn", {x: 600, y: 250, duration: 3, ease: "power1", opacity: 1, delay: 8})
 
-gsap.from(".no-btn", {x: 740, y: 1000, opacity: 0, delay: 8})
-gsap.to(".no-btn", {x: 740, y: 250, duration: 3, ease: "power1", opacity: 1, delay: 8})
+gsap.from(".no-btn", {x: 700, y: 1000, opacity: 0, delay: 8})
+gsap.to(".no-btn", {x: 700, y: 250, duration: 3, ease: "power1", opacity: 1, delay: 8})
 
 let no_btn = document.querySelector(".no-btn")
-no_btn.addEventListener("mouseover", () => {
-    no_btn.textContent = "YES";
-})
-no_btn.addEventListener("mouseout", () => {
-    no_btn.textContent = "NO";
-})
+no_btn.addEventListener("mouseover", () => no_btn.textContent = "YES")
+no_btn.addEventListener("mouseout", () => no_btn.textContent = "NO")
+
+let smile = document.querySelector("#smile")
+const rotate = gsap.to(smile, {rotation: 360})
+smile.addEventListener("mouseenter", () => rotate.play())
+smile.addEventListener("mouseleave", () => rotate.reverse())
