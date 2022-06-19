f46608134cbf87043468d06dbe7edb07aaceb226
Elena-Weber <crochetmiracles@gmail.com>
2022-06-19 11:58:18 -0400

    add thanksgiving card

---
+++ card/js/main.js
@@ -0,0 +1,21 @@
+/*  Autumn Greeting Card -- js */
+
+(function($){
+	'use strict';
+
+	// declare actors here
+
+	// clear stage 
+
+	// enter floor vegetation
+
+	// enter tree
+
+	// enter the greeting text
+	
+	// the GO function ...to kick things all off
+	
+
+})(jQuery);
+
+
