02be4bbf1a145b17d30f5e5cae90f06e1f591d35
Elena-Weber <crochetmiracles@gmail.com>
2022-06-19 22:50:24 -0400

    bg change and leaves fall

--- card/js/main.js
+++ card/js/main.js
@@ -1,10 +1,9 @@
-/*  Autumn Greeting Card -- js */
-
 (function($){
 	'use strict';
 
-	// declare actors here - jQuery objects
-	var $backFallingLeaves = $('#brownLeaf, #orangeLeaf, #redLeaf'),
+	// jQuery objects
+	var $body = $('body'),
+		$backFallingLeaves = $('#brownLeaf, #orangeLeaf, #redLeaf'),
 		$textLine1 = $('.text-line-1'),
 		$textLine2 = $('.text-line-2'),
 		$textGreeting = $('.text-greeting'),
@@ -17,41 +16,41 @@
 		$tree = $('#tree_trunk'),
 		$cardContainer = $('.card.container')
 
-	// clear stage - hiding all of them in the very beginning
+	// hide all objects from start
 	function clearStage() {
 		var clearTimeline = gsap.timeline();
 		clearTimeline
-			.set($backFallingLeaves, {autoAlpha: 0})
+			.set($backFallingLeaves, {autoAlpha: 0}) // make invisible
 			.set($textLine1, {autoAlpha: 0})
 			.set($textLine2, {autoAlpha: 0})
 			.set($textGreeting, {autoAlpha: 0})
 			.set($treeLeaves, {autoAlpha: 0})
-			.set($bird, {y:'+=65', autoAlpha: 0}) // move the bird 65px down so it's hidden by the nest
+			.set($bird, {y:'+=65', autoAlpha: 0}) // move bird 65px down so it's hidden by nest
 			.set($nest, {autoAlpha: 0})
 			.set($tree, {autoAlpha: 0})
-			.set($floorLeaves, {y:'+=275', onComplete: showContainer}) // move the tree 275px down so it's hidden out of the screen and call the function after it's completed
-
+			.set($floorLeaves, {y:'+=275', onComplete: showContainer}) // move tree 275px down so it's hidden beyond screen and call function below after it's completed
 		;
 
 		function showContainer() {
-			$cardContainer.css('display', 'block');
+			$cardContainer.css('display', 'block'); // display all
 		}
 
 		return clearTimeline;
 	}
 
-	// enter floor vegetation
+	// leaves on bottom
 	function enterFloorVegetation() {
 		var floorLeavesTimeline = gsap.timeline();
 		floorLeavesTimeline
 			.staggerTo($floorLeaves, 1, {y:0, ease: Back.easeInOut}, 0.01)
 			.fromTo($tree, 1.1, {scaleY:0.2, autoAlpha:0, transformOrigin:'center bottom'}, {scaleY:1, autoAlpha:1, transformOrigin:'center bottom', ease: Back.easeInOut}) // tree grows from bottom/floor, longer on Y
 			.fromTo($tree, 0.9, {scaleX:0.2, autoAlpha:0, transformOrigin:'center bottom'}, {scaleX:1, autoAlpha:1, transformOrigin:'center bottom', ease: Back.easeInOut}, '-=0.9') // tree grows from bottom/floor, shorter on X
+		;
 
 		return floorLeavesTimeline;
 	}
 
-	// enter tree leaves, bird, nest
+	// tree leaves, bird, nest
 	function enterTreeStuff() {
 		var treeStuffTimeline = gsap.timeline();
 		treeStuffTimeline
@@ -63,7 +62,7 @@
 			.to($bird, 1.4, {y:'-=39', autoAlpha: 1, ease: Power4.easeInOut}, 'nest-pop-in+=0.1')
 			.add('bird-peeking')
 			.set($birdEyes, {autoAlpha:0})
-			.set($birdEyes, {autoAlpha:1}, '=+0.2')
+			.set($birdEyes, {autoAlpha:1}, '=+0.2') // adjust delay
 			.set($birdEyes, {autoAlpha:0}, '+=0.3')
 			.set($birdEyes, {autoAlpha:1}, '+=0.2')
 			.add('bird-blinks')
@@ -71,47 +70,69 @@
 			.to($bird, 0.3, {y:'+=8', ease: Back.easeOut})
 			.to($birdHat, 0.4, {y:'-=12'}, '-=0.6')
 			.to($birdHat, 0.3, {y:0, rotation:0, x:0, onComplete: startBlinking}, '-=0.2')
-			;
+		;
 
-			function startBlinking() {
-				var birdBlinksTimeline = gsap.timeline({repeat: -1, repeatDelay: 5}); // creating a loop
-				birdBlinksTimeline
-					.set($birdEyes, {autoAlpha:0})
-					.set($birdEyes, {autoAlpha:1}, '=+0.2')
-					.set($birdEyes, {autoAlpha:0}, '+=1.2')
-					.set($birdEyes, {autoAlpha:1}, '+=0.2')
+		function startBlinking() {
+			var birdBlinksTimeline = gsap.timeline({repeat: -1, repeatDelay: 5}); // creating a loop
+			birdBlinksTimeline
+				.set($birdEyes, {autoAlpha:0})
+				.set($birdEyes, {autoAlpha:1}, '=+0.2')
+				.set($birdEyes, {autoAlpha:0}, '+=1.2')
+				.set($birdEyes, {autoAlpha:1}, '+=0.2')
 
-				return birdBlinksTimeline;
-			}
+			return birdBlinksTimeline;
+		}
 
 		return treeStuffTimeline;
 	}
 
-	// enter the greeting text
+	// text
 	function enterGreeting() {
 		var greetingTimeline = gsap.timeline();
 		greetingTimeline
-			.fromTo($textLine1, 1, {y:'-=50', autoAlpha: 0}, {y:0, autoAlpha: 1})
+			.fromTo($textLine1, 1, {y:'-=50', autoAlpha: 0}, {y:0, autoAlpha: 1, onComplete: startLoops})
 			.fromTo($textLine2, 1, {y:'-=25', autoAlpha: 0}, {y:0, autoAlpha: 1})
-			.staggerFromTo($textGreeting, 0.5, {scale:2, autoAlpha:0, transformOrigin: 'center center'}, {scale:1, autoAlpha:1, transformOrigin: 'center center'}, 0.1)
+			.staggerFromTo($textGreeting, 0.5, {scale:2, autoAlpha:0, transformOrigin: 'center center'}, {scale:1, autoAlpha:1, transformOrigin: 'center center'}, 0.1);
+
+		function startLoops() {
+			var colors = ['#edcc93', '#f7e3ae', '#f3ebcc', '#edcc93'];
+			var backgroundTimeline = gsap.timeline({repeat:-1, repeatDelay:2});
+			backgroundTimeline
+				.to($body, 3, {backgroundColor: colors[0]})
+				.to($body, 3, {backgroundColor: colors[1]}, '+=2')
+				.to($body, 3, {backgroundColor: colors[2]}, '+=2')
+				.to($body, 3, {backgroundColor: colors[3]}, '+=2');
+
+			gsap.set($backFallingLeaves, {y:-100, autoAlpha:0.2});
+			gsap.to("#brownLeaf", 10+Math.random()*10, {y:'+=1200', autoAlpha:1, ease: Linear.easeNone, onComplete: fallingLeaves, onCompleteParams: ['#brownLeaf']});
+			gsap.to("#redLeaf", 10+Math.random()*10, {y:'+=1200', autoAlpha:1, ease: Linear.easeNone, onComplete: fallingLeaves, onCompleteParams: ['#redLeaf']});
+			gsap.to("#orangeLeaf", 10+Math.random()*10, {y:'+=1200', autoAlpha:1, ease: Linear.easeNone, onComplete: fallingLeaves, onCompleteParams: ['#orangeLeaf']});
+
+			function fallingLeaves(leaf) {
+				var range = Math.random()*800,
+					offset = 400,
+					newPosition = range-offset;
+
+				gsap.set(leaf, {x: newPosition, y:-100, autoAlpha: 0.2, rotation: Math.random()*360})
+				gsap.to(leaf, 10+Math.random()*10, {y:'+=1200', autoAlpha:1, ease: Linear.easeNone, onComplete: fallingLeaves, onCompleteParams: [leaf]});
+			}
+		}
 
 		return greetingTimeline;
 	}
 	
-	// the GO function ...to kick things all off
+	// starting function
 	function go() {
-		console.log("start");
+		// console.log("start");
 		var masterTimeline = gsap.timeline();
 		masterTimeline
 			.add(clearStage(), 'scene-clear-stage')
 			.add(enterFloorVegetation(), 'scene-floor-vegetation')
 			.add(enterTreeStuff(), 'scene-tree-stuff')
 			.add(enterGreeting(), 'scene-greeting')
-			;
+		;
 	}
 
 	go()
 
 })(jQuery);
-
-
